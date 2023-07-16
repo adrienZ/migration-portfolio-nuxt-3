61cfdab0e08b176f9fd86f41e8b4e30c21568733
Adrien Zaganelli <adrienzaganelli@gmail.com>
2023-07-16 13:18:26 +0200

    moar pages

--- tests/migration.spec.ts
+++ tests/migration.spec.ts
@@ -1,17 +1,30 @@
 import { test, expect } from '@playwright/test';
 
-test('Homepage', async ({ page, browser }) => {
-  await page.goto("/");
 
-  await page.waitForLoadState('networkidle');
-  await page.waitForTimeout(2000);
+test.describe('index pages', () => {
 
-  await page.setViewportSize({
-    width: page.viewportSize()?.width || 0,
-    height: 3000
+  const pages = [
+    ["Homepage", "/"],
+    ["Blog", "/blog"],
+    ["2018", "/2018"],
+  ]
+
+  pages.forEach(([ name, url]) => {
+    test(name, async ({ page }) => {
+      await page.goto(url);
+    
+      await page.waitForLoadState('networkidle');
+      await page.waitForTimeout(2000);
+    
+      await page.setViewportSize({
+        width: page.viewportSize()?.width || 0,
+        height: 3000
+      })
+    
+      await expect(page).toHaveScreenshot({
+        fullPage: true, 
+      })
+    });
   })
+})
 
-  await expect(page).toHaveScreenshot({
-    fullPage: true, 
-  })
-});
