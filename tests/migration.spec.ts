0de55da442839d3ad17d9388d5c110b66a68ff76
Adrien Zaganelli <adrienzaganelli@gmail.com>
2023-07-16 12:01:29 +0200

    init

---
+++ tests/migration.spec.ts
@@ -0,0 +1,17 @@
+import { test, expect } from '@playwright/test';
+
+test('Homepage', async ({ page, browser }) => {
+  await page.goto("/");
+
+  await page.waitForLoadState('networkidle');
+  await page.waitForTimeout(2000);
+
+  await page.setViewportSize({
+    width: page.viewportSize()?.width || 0,
+    height: 4000
+  })
+
+  await expect(page).toHaveScreenshot({
+    fullPage: true, 
+  })
+});
