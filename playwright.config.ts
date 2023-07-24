d49c4932ec9f7e9f5ade6476ee21d441eff7d05b
Adrien Zaganelli <adrienzaganelli@gmail.com>
2023-07-24 16:18:45 +0200

    reporter html

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,6 +23,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   // reporter: process.env.CI ? 'dot' : 'list',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  reporter: 'html',
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'https://adrienzaganelli.com',
